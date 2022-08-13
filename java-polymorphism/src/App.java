887350ccafb35a6b22a04694ced4c09468a889f7
github username <um_2003_8@hotmail.com>
2022-08-13 12:38:32 -0400

    add a short notes about different form of methods

--- java-polymorphism/src/App.java
+++ java-polymorphism/src/App.java
@@ -24,11 +24,14 @@ public class App {
 	public static void main(String[] args) {
 		Bird b = new Bird();
 		b.sing();
+		//displays tweet tweet tweet
 		
 		Robin r = new Robin();
 		r.sing();
+		//also displays tweet tweet tweet because it doesn't have its own method but it is inheriting from Bird class.
 		
 		Pelican p = new Pelican();
 		p.sing();
+		//displays kwah kwah kwah. Because of polymorphism even it is inheriting from a bird class it displays the sing method from its own class.		
 	}
 }
