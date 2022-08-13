f3a823102dbccf6274ffcf412c8d1f4f801a07a1
github username <um_2003_8@hotmail.com>
2022-08-13 12:18:29 -0400

    create a class Bird and add a method sing

--- java-polymorphism/src/App.java
+++ java-polymorphism/src/App.java
@@ -2,6 +2,12 @@
  	Polymorphism means "many forms", and it occurs when we have many classes that are related to each other by inheritance.
  */
 
+class Bird {
+	public void sing() {
+		System.out.println("tweet tweet tweet");
+	}
+}
+
 public class App {
 	public static void main(String[] args) {
 
