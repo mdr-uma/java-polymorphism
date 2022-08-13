cefe5d10d7012f9ff8a2120b2be1715a6f23cda9
github username <um_2003_8@hotmail.com>
2022-08-13 12:17:45 -0400

    add a note about a polymorphism

--- java-polymorphism/src/App.java
+++ java-polymorphism/src/App.java
@@ -1,3 +1,6 @@
+/*
+ 	Polymorphism means "many forms", and it occurs when we have many classes that are related to each other by inheritance.
+ */
 
 public class App {
 	public static void main(String[] args) {
